bfc51e69872d1bb7219a0f4a1c7e69958a882aab
mastayb <bmastay@gmail.com>
2017-09-28 17:11:52 -0400

    Further speedup using a tuple of the offset and a class method for wrapping instead of generating all of the new indices

--- grid.py
+++ grid.py
@@ -1,38 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 import curses
-import itertools
-
-def wrapped_range(start, stop, limit, minimum=0):
-    #generator that wraps at 0 and limit
-    for i in range(start,0):
-        yield limit+i
-    for i in range(max(start,0), min(stop,limit)):
-        yield i
-    for i in range(limit, stop):
-        yield i-limit
-
-
-def wrapped_neighbor_range(r,c,rows,columns):
-    for nr in wrapped_range(r-1,r+2,rows):
-        for nc in wrapped_range(c-1,c+2,columns):
-            if (nr,nc) == (r,c):
-                continue
-            else:
-                yield (nr,nc)
-
 class Grid:
     def __init__(self, rows, columns):
         self.grid = [[0 for _ in range(columns)] for _ in range(rows)]
         self.__num_rows = rows
         self.__num_columns = columns
 
+        self.offsets = ((-1,-1), (-1,0), (-1,1), (0,-1), (0,1), (1,-1), (1,0), (1,1))
+
     def update(self):
         newGrid = [[0 for _ in range(self.__num_columns)] for _ in range(self.__num_rows)]
         for r in range(len(self.grid)):
             for c in range(len(self.grid[r])):
-                live_neighbors = sum(self.grid[nr][nc] 
-                        for (nr,nc) in wrapped_neighbor_range(r,c,self.__num_rows, self.__num_columns)) 
+                live_neighbors = sum(self.cell_state(nr,nc) for (nr,nc) in map(lambda o: (r+o[0], c+o[1]), self.offsets))
 
                 if self.grid[r][c] and live_neighbors in range(2,4):
                     newGrid[r][c] = 1
@@ -66,6 +47,13 @@ class Grid:
         self.__num_columns = new_c
 
 
+    def cell_state(self, r, c):
+        while r >= self.__num_rows:
+            r -= self.__num_rows
+        while c >= self.__num_columns:
+            c -= self.__num_columns
+        return self.grid[r][c]
+    
 
         
     def set_alive(self, row, column):
